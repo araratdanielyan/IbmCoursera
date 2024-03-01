3fe771c8a2c7eb5835c5cbd5406766831bba3180
araratdanielyan <araratdanielyan@gmail.com>
2024-03-01 03:03:41 -0500

    fourth commit

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -4,6 +4,8 @@ let isLoggedIn = true;
 let userMessage;
 let userType = "subscriber";
 let userCategory;
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 if (userRole === "admin") {
     accessLevel = "Full access granted";
@@ -42,3 +44,5 @@ console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 
 console.log("User Category:", userCategory);
+
+console.log("Authentication Status:", authenticationStatus);

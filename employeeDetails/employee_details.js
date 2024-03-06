de0ac18bba8ca89374b32648083edddb98c78082
araratdanielyan <araratdanielyan@gmail.com>
2024-03-05 20:25:19 -0500

    specialization addition

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,13 +1,17 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: "C#" },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: "JavaScript" },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: "Java" },
     //... More employee records can be added here
   ];
 
  // Function to display all employees
 const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
 document.getElementById('employeesDetails').innerHTML = totalEmployees;
+
+function displayEmployees(){
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
 
 function calculateTotalSalaries() {
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
@@ -27,5 +31,14 @@ function findEmployeeById(employeeId) {
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+
+function findEmployeeBySpec(){
+    const foundEmployee = employees.find(employee => employee.specialization === "JavaScript");
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.specialization}</p>`;
+    } else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
     }
 }
